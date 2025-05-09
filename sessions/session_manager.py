38aa561f27581b4cd707313fba1f911c6e65b3b6
kyle006 <kyle.jeremy.meier@gmail.com>
2025-05-09 09:44:08 +0200

    Session basic created

--- sessions/session_manager.py
+++ sessions/session_manager.py
@@ -1,1 +1,12 @@
 # TODO: start sessions and end them.
+import podman
+from podman import PodmanClient
+
+IMAGE="ubuntu:24.04"
+sessions={}
+
+def start_session(user_id: str):
+    client= PodmanClient.from_env()
+    container = client.containers.create(IMAGE, detach=True)
+    container.start()
+    sessions[user_id] = container
